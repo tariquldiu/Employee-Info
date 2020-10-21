73aa9629ade2c8aa63cd8b691353f16fd4d97e18
tariqul.diu.cse@gmail.com <tariqul.diu.cse@gmail.com>
2020-10-21 14:25:44 +0600

    EmployeeInfo

---
+++ EmployeeInfo/Controllers/EmployeeController.cs
@@ -0,0 +1,121 @@
+﻿using EmployeeInfo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EmployeeInfo.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public JsonResult Get_AllCity()
+        {
+            using (EmployeeInfoContext Obj = new EmployeeInfoContext())
+            {
+              
+                var citis = (from c in Obj.Cities
+                                 select new
+                                 { c.CityId, c.CityName})
+                                 .ToList();
+                return Json(citis, JsonRequestBehavior.AllowGet);
+            }
+        }
+        public JsonResult Get_AllDepartment()
+        {
+            using (EmployeeInfoContext Obj = new EmployeeInfoContext())
+            {
+                var department = (from d in Obj.Departments
+                                 select new
+                                 { d.DeptId, d.DeptName })
+                                 .ToList();
+                return Json(department, JsonRequestBehavior.AllowGet);
+            }
+        }
+        public JsonResult Get_AllEmployee()
+        {
+            using (EmployeeInfoContext Obj = new EmployeeInfoContext())
+            {
+                var employees = (from e in Obj.Employees
+                                select new
+                                { e.EmpId, e.EmpName, e.Gender, e.DeptId, e.CityId, e.Department.DeptName, e.City.CityName } )
+                                .ToList();
+                return Json(employees, JsonRequestBehavior.AllowGet);
+            }
+        }
+       
+        public JsonResult Get_EmployeeById(string Id)
+        {
+            using (EmployeeInfoContext Obj = new EmployeeInfoContext())
+            {
+                int EmpId = int.Parse(Id);
+                return Json(Obj.Employees.Find(EmpId), JsonRequestBehavior.AllowGet);
+            }
+        }
+      
+        public string Insert_Employee(Employee Employe)
+        {
+            if (Employe != null)
+            {
+                using (EmployeeInfoContext Obj = new EmployeeInfoContext())
+                {
+                    Obj.Employees.Add(Employe);
+                    Obj.SaveChanges();
+                    return "Employee Added Successfully";
+                }
+            }
+            else
+            {
+                return "Employee Not Inserted! Try Again";
+            }
+        }
+       
+        public string Delete_Employee(Employee Emp)
+        {
+            if (Emp != null)
+            {
+                using (EmployeeInfoContext Obj = new EmployeeInfoContext())
+                {
+                    var Emp_ = Obj.Entry(Emp);
+                    if (Emp_.State == System.Data.Entity.EntityState.Detached)
+                    {
+                        Obj.Employees.Attach(Emp);
+                        Obj.Employees.Remove(Emp);
+                    }
+                    Obj.SaveChanges();
+                    return "Employee Deleted Successfully";
+                }
+            }
+            else
+            {
+                return "Employee Not Deleted! Try Again";
+            }
+        }
+      
+        public string Update_Employee(Employee Emp)
+        {
+            if (Emp != null)
+            {
+                using (EmployeeInfoContext Obj = new EmployeeInfoContext())
+                {
+                    var Emp_ = Obj.Entry(Emp);
+                    Employee EmpObj = Obj.Employees.Where(x => x.EmpId == Emp.EmpId).FirstOrDefault();
+                    EmpObj.EmpName = Emp.EmpName;
+                    EmpObj.DeptId = Emp.DeptId;
+                    EmpObj.CityId = Emp.CityId;
+                    Obj.SaveChanges();
+                    return "Employee Updated Successfully";
+                }
+            }
+            else
+            {
+                return "Employee Not Updated! Try Again";
+            }
+        }
+    }
+}
---
+++ EmployeeInfo/Scripts/AngularjsCode.js
@@ -0,0 +1,103 @@
+﻿var app = angular.module("myApp", []);
+app.controller("myCtrl", function ($scope, $http) {
+    Clear();
+    function Clear() {
+        $scope.Employee = {};
+        $scope.Employee.EmpId = 0;
+        $scope.EmployeeList = [];
+        $scope.DepartmentList = [];
+        $scope.CityList = [];
+        GetAllDepartment();
+        GetAllCity();
+    }
+
+  
+    $scope.InsertData = function () {
+        var Action = document.getElementById("btnSave").getAttribute("value");
+        if (Action == "Submit") {
+           
+            $http({
+                method: "post",
+                url: "http://localhost:7126/Employee/Insert_Employee",
+                datatype: "json",
+                data: JSON.stringify($scope.Employee)
+            }).then(function (response) {
+                alert(response.data);
+                $scope.GetAllData();
+                $scope.Employee.EmpName = "";
+                $scope.Employee.Gender = "";
+                $scope.Employee.DeptName = "";
+                $scope.Employee.CityName = "";
+            })
+        } else {
+           
+            $scope.Employee.EmpId = document.getElementById("EmpID_").value;
+            $http({
+                method: "post",
+                url: "http://localhost:7126/Employee/Update_Employee",
+                datatype: "json",
+                data: JSON.stringify($scope.Employee)
+            }).then(function (response) {
+                alert(response.data);
+                $scope.GetAllData();
+                $scope.Employee.EmpName = "";
+                $scope.Employee.Gender = "";
+                $scope.Employee.DeptName = "";
+                $scope.Employee.CityName = "";
+                document.getElementById("btnSave").setAttribute("value", "Submit");
+                document.getElementById("btnSave").style.backgroundColor = "cornflowerblue";
+                document.getElementById("spn").innerHTML = "Add New Employee";
+            })
+        }
+    }
+    function GetAllCity(){
+        $http({
+            method: "get",
+            url: "http://localhost:7126/Employee/Get_AllCity"
+        }).then(function (response) {
+            $scope.CityList = response.data;
+            console.log($scope.CityList);
+        }, function () {
+            alert("Error Occur");
+        })
+    }
+    function GetAllDepartment() {
+        $http({
+            method: "get",
+            url: "http://localhost:7126/Employee/Get_AllDepartment"
+        }).then(function (response) {
+            $scope.DepartmentList = response.data;
+            console.log($scope.DepartmentList);
+        }, function () {
+            alert("Error Occur");
+        })
+    }
+    $scope.GetAllData = function () {
+        $http({
+            method: "get",
+            url: "http://localhost:7126/Employee/Get_AllEmployee"
+        }).then(function (response) {
+            $scope.EmployeeList = response.data;
+        }, function () {
+            alert("Error Occur");
+        })
+    };
+    $scope.DeleteEmp = function (Emp) {
+        $http({
+            method: "post",
+            url: "http://localhost:7126/Employee/Delete_Employee",
+            datatype: "json",
+            data: JSON.stringify(Emp)
+        }).then(function (response) {
+            alert(response.data);
+            $scope.GetAllData(); 
+        })
+    };
+    $scope.UpdateEmp = function (Emp) {
+        document.getElementById("EmpID_").value = Emp.EmpId;
+        $scope.Employee = Emp;
+        document.getElementById("btnSave").setAttribute("value", "Update");
+        document.getElementById("btnSave").style.backgroundColor = "Yellow";
+        document.getElementById("spn").innerHTML = "Update Employee Information";
+    }
+})  
